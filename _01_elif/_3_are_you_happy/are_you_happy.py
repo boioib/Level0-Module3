0fd453183add7ab535f3a10a0e7148ab5cb496bc
boioib <109253331+boioib@users.noreply.github.com>
2022-07-15 16:25:10 -0700

    Update are_you_happy.py

--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -4,5 +4,22 @@ from tkinter import Tk, simpledialog, messagebox
 if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
+    window=Tk()
+    window.withdraw()
+    main_answer=simpledialog.askstring(title='Question', prompt='Are you happy?')
+    if main_answer == ("yes"):
+        messagebox.showinfo(message='Keep doing whatever you are doing')
+    elif main_answer == ("no"):
+        answer_2=simpledialog.askstring(title='Question', prompt='Do you want to be happy?')
+        if answer_2 == ("no"):
+            messagebox.showinfo(message='Keep doing whatever you are doing')
+        elif answer_2 == ("yes"):
+            messagebox.showinfo(message='Change something')
 
-    pass
+
+
+
+
+
+
+pass
